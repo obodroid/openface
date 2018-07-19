fb7585a66b44381599f197192c9529e2850bee10
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2018-07-19 13:19:33 +0700

    visualize face rep

---
+++ demos/visualize_rep.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python2
+import time
+
+start = time.time()
+
+import argparse
+import cv2
+import itertools
+import os
+
+import numpy as np
+np.set_printoptions(precision=2)
+
+import openface
+
+fileDir = os.path.dirname(os.path.realpath(__file__))
+modelDir = os.path.join(fileDir, '..', 'models')
+dlibModelDir = os.path.join(modelDir, 'dlib')
+openfaceModelDir = os.path.join(modelDir, 'openface')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('imgs', type=str, nargs='+', help="Input images.")
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",
+                    default=os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'))
+parser.add_argument('--imgDim', type=int,
+                    help="Default image dimension.", default=96)
+parser.add_argument('--verbose', action='store_true')
+
+args = parser.parse_args()
+
+if args.verbose:
+    print("Argument parsing and loading libraries took {} seconds.".format(
+        time.time() - start))
+
+start = time.time()
+align = openface.AlignDlib(args.dlibFacePredictor)
+net = openface.TorchNeuralNet(args.networkModel, args.imgDim)
+if args.verbose:
+    print("Loading the dlib and OpenFace models took {} seconds.".format(
+        time.time() - start))
+
+def getRep(imgPath):
+    start = time.time()
+    if args.verbose:
+        print("Processing {}.".format(imgPath))
+    
+    bgrImg = cv2.imread(imgPath)
+
+    if bgrImg is None:
+        raise Exception("Unable to load image/frame")
+
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+    if args.verbose:
+        print("  + Original size: {}".format(rgbImg.shape))
+    if args.verbose:
+        print("Loading the image took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+    bbs = align.getAllFaceBoundingBoxes(rgbImg)
+
+    if args.verbose:
+        print("Face detection took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+
+    alignedFaces = []
+    for box in bbs:
+        alignedFaces.append(
+            align.align(
+                args.imgDim,
+                rgbImg,
+                box,
+                landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE))
+
+    if alignedFaces is None:
+        raise Exception("Unable to align the frame")
+    if args.verbose:
+        print("Alignment took {} seconds.".format(time.time() - start))
+
+    start = time.time()
+
+    reps = []
+    for alignedFace in alignedFaces:
+        reps.append(net.forward(alignedFace))
+
+    if args.verbose:
+        print("Neural network forward pass took {} seconds.".format(
+            time.time() - start))
+
+    for bb in bbs:
+        cv2.rectangle(bgrImg,(bb.left(), bb.top()), (bb.right(), bb.bottom()),(0,255,0),2)
+    cv2.imshow('image', bgrImg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return (reps,bbs)
+
+for img in args.imgs:
+    repsAndBBs = getRep(img)
+    reps = repsAndBBs[0]
+    bbs = repsAndBBs[1]
+    break
