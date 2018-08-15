b244819a66f0f9610d3b97664322b5ab6a54f01e
Jiradej Vatanutanon <jiradej_cu@hotmail.com>
2018-08-15 12:42:16 +0700

    generate tsne for many perplexity

--- util/tsne.py
+++ util/tsne.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python2
+import ptvsd
+
+# Allow other computers to attach to ptvsd at this IP address and port, using the secret
+# ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
+# Pause the program until a remote debugger is attached
+# ptvsd.wait_for_attach()
 
 import numpy as np
 import pandas as pd
@@ -26,23 +32,26 @@ parser.add_argument('workDir', type=str)
 parser.add_argument('--names', type=str, nargs='+', required=True)
 args = parser.parse_args()
 
-y = pd.read_csv("{}/labels.csv".format(args.workDir)).as_matrix()[:, 0]
-X = pd.read_csv("{}/reps.csv".format(args.workDir)).as_matrix()
+y = pd.read_csv("{}/labels.csv".format(args.workDir), header=None).as_matrix()[:, 0]
+X = pd.read_csv("{}/reps.csv".format(args.workDir), header=None).as_matrix()
 
 target_names = np.array(args.names)
 colors = cm.Dark2(np.linspace(0, 1, len(target_names)))
 
-X_pca = PCA(n_components=50).fit_transform(X, X)
-tsne = TSNE(n_components=2, init='random', random_state=0)
-X_r = tsne.fit_transform(X_pca)
+nc = None if len(X) < 50 else 50
+X_pca = PCA(n_components=nc).fit_transform(X, X)
+for p in [2,5,10,30,50,100]:
+    tsne = TSNE(n_components=2, init='random', random_state=0, perplexity=p)
+    X_r = tsne.fit_transform(X_pca)
 
-for c, i, target_name in zip(colors,
-                             list(range(1, len(target_names) + 1)),
-                             target_names):
-    plt.scatter(X_r[y == i, 0], X_r[y == i, 1],
-                c=c, label=target_name)
-plt.legend()
+    plt.figure()
+    for c, i, target_name in zip(colors,
+                                list(range(1, len(target_names) + 1)),
+                                target_names):
+        plt.scatter(X_r[y == i, 0], X_r[y == i, 1],
+                    c=c, label=target_name)
+    plt.legend()
 
-out = "{}/tsne.pdf".format(args.workDir)
-plt.savefig(out)
-print("Saved to: {}".format(out))
+    out = "{}/tsne_{}.pdf".format(args.workDir, p)
+    plt.savefig(out)
+    print("Saved to: {}".format(out))
