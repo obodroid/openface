dd8fdffb4b78cb4e98eb77e1313f7b5a40833004
Jiradej Vatanutanon <jiradej.v@hiveground.com>
2018-11-26 19:33:28 +0700

    Revert "return label before predict people"
    This reverts commit ad3f7be7101240a5857b2a1cbcaab6c287ad2b49.

--- server/websocket-server.py
+++ server/websocket-server.py
@@ -528,11 +528,11 @@ class OpenFaceServerProtocol(WebSocketServerProtocol):
         if face.cluster == self.faceId:
             self.faceId += 1
 
-        if face.label:
-            msg["label"] = face.label
-        elif face.identity:
+        if face.identity:
             msg["predict_name"] = face.label
             msg["predict_people_id"] = face.identity
+        elif face.label:
+            msg["label"] = face.label
 
         self.sendMessage(json.dumps(msg))
 
